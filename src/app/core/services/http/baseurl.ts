e6b17f452d0d5c89c6bd19777fa2d96d7478b518
cridargrajales <cridargrajales@utp.edu.co>
2024-11-13 15:14:47 -0500

    Actualiza la URL base y actualiza las dependencias a versiones más recientes

--- src/app/core/services/http/baseurl.ts
+++ src/app/core/services/http/baseurl.ts
@@ -1,6 +1,4 @@
-export const URL =
-  'https://6w0f3dx0-3060.use2.devtunnels.ms/api/soportebot/v1';
+export const URL = 'https://ltkgvh8b-3060.use2.devtunnels.ms/api/soportebot/v1';
 
 export const BASEURL =
-  'https://6w0f3dx0-3060.use2.devtunnels.ms/api/soportebot/v1';
-
+  'https://ltkgvh8b-3060.use2.devtunnels.ms/api/soportebot/v1';
